407d5d11a148500f79c4acc3b57acd91753aebf6
RonAlmog <ron8132@gmail.com>
2025-01-12 18:54:09 -0500

    minor problems

--- scripts/buildings/smelter.gd
+++ scripts/buildings/smelter.gd
@@ -18,5 +18,19 @@ func initialize(rotation: int):
 	pass
 	
 
-#func can_accept_item(item: Item, input_direction: Vector2):
-	#pass
+func can_accept_item(item: Item, input_direction: Vector2) -> bool:
+	if item.type == ItemManager.COAL_ORE && has_coal == true:
+			return false
+	
+	if is_empty() && !is_generating && inputs.has(input_direction) && item.type % 10 == inputs[input_direction]:
+		return true
+	return false
+	
+func input_item():
+	output_type = stored_item.type
+	
+	ItemManager.delete_item(stored_item)
+	stored_item = null
+	
+	tick_counter = 0
+	is_generating = true
